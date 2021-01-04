324b3b0fe80137f213a47e8b52837f0853db9c52
victorine <victorine.maurel@hotmail.fr>
2021-01-04 14:22:11 +0100

    first commit for the results

--- src/components/PatientResults.vue
+++ src/components/PatientResults.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="patientResults">
     <h1>Résultats du patient</h1>
+    <h2>Nom du patient - date de la consultation</h2>
+    <vs-button color="#9082FF" type="filled" v-on:click="download" id="btnDownload">Télécharger les résultats</vs-button>
+    <div class="testMotricité">
+      <div class="testMotricité-header">
+        <h3>Test motricité fine</h3>
+        <span> - réussi à 55%</span>
+        <span> - non validé</span>
+      </div> 
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'PatientResults'
+  name: 'PatientResults',
+  methods: {
+    download(){
+      console.log("coucou")
+    }
+  }
 }
 </script>
 
@@ -18,5 +32,8 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+}
+#btnDownload{
+  border-radius: 16px;
 }
 </style>

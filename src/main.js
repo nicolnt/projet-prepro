f591a188df5a00d2cd4369266b47ebb801264f2e
Nicolas LIENART <nicolas@lienart.net>
2021-02-14 14:42:24 +0100

    test send results to db

--- src/main.js
+++ src/main.js
@@ -2,15 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import Vuesax from 'vuesax'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 import 'vuesax/dist/vuesax.css' //Vuesax styles
 import 'material-icons/iconfont/material-icons.css'
+import store from "./services/stores/logStore";
 
 Vue.config.productionTip = false
 
 Vue.use(Vuesax);
 
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')

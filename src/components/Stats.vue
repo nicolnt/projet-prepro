345bf02c9e407ea903aef795e1d0f2ed54d990eb
Unknown <veysset.sarah@gmail.com>
2021-02-22 20:19:24 +0100

    Add graph statistics

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,17 +1,61 @@
 <template>
   <div class="stats">
     <Hero title="Statistiques des patients"/>
+    <div id="chart">
+      <apexchart type="area" width="800" height="500" :options="chartOptions" :series="series"></apexchart>
+    </div>
   </div>
 </template>
 
 <script>
 import Hero from '@/components/Hero.vue'
 
+import VueApexCharts from 'vue-apexcharts'
+
 export default {
   name: 'Stats',
   components: {
     Hero,
-  }
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      series: 
+      [{
+        name: '2021',
+        data: [31, 40, 28, 51, 42, 109, 100, 60, 86, 45, 100, 120]
+      }, 
+      {
+        name: '2022',
+        data: [11, 32, 45, 32, 34, 52, 41, 51, 42, 109, 100, 42]
+      }],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        colors: ["#9082FF", "#FF8D8B"],
+        xaxis: {
+          // type: 'datetime',
+          categories: ['Janvier','Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre' , 'Novembre', 'Décembre']
+        },
+        // tooltip: {
+        //   x: {
+        //     format: 'dd/MM/yy HH:mm'
+        //   },
+        // },    
+      },
+      legend: {
+        fontSize: '16px',
+      },
+    }
+  },
 }
 </script>
 
@@ -28,5 +72,8 @@ export default {
   display: flex;
   align-self: flex-start;
   margin-top: 12px;
+}
+#chart {
+  margin-top: 2rem;
 }
 </style>

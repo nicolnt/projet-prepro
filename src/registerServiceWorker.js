5aab8ed864bba850b315d300c6f20e1835be04e5
Sophie <sophbyr@gmail.com>
2021-03-17 09:40:59 +0100

    [+] Add PWA plugin

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-console */
+
 import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
@@ -25,3 +29,4 @@ import { register } from 'register-service-worker'
       console.error('Error during service worker registration:', error)
     }
   })
+}
